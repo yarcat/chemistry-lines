e41a7a5834f45deae20e67d58eb653ca468ee256
Jaroslavs Samcuks <yarcat@gmail.com>
2012-08-31 01:29:29 +0200

    SlideTouchLogic implements View.OnTouchListener

--- Chemistry Lines/src/com/yarcat/chemistrylines/touchlogic/SlideTouchLogic.java
+++ Chemistry Lines/src/com/yarcat/chemistrylines/touchlogic/SlideTouchLogic.java
@@ -1,34 +1,11 @@
 package com.yarcat.chemistrylines.touchlogic;
 
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
+import android.view.View;
 
-public class SlideTouchLogic extends SimpleOnGestureListener {
+public class SlideTouchLogic implements View.OnTouchListener {
 
-    private FieldView mFieldView;
-
-    public SlideTouchLogic(FieldView fieldHelper) {
-        mFieldView = fieldHelper;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-            float distanceY) {
-        int index = mFieldView.getIndex(e2.getX(), e2.getY());
-        mFieldView.select(index, false);
-        return true;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-        int index = mFieldView.getIndex(e.getX(), e.getY());
-        mFieldView.select(index, true);
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-            float velocityY) {
-        mFieldView.clean();
+    public boolean onTouch(View v, MotionEvent event) {
         return true;
     }
 }
