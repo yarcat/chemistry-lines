5325a88a0fbcbf3bb774cf9156458551f5ba17b7
Jaroslavs Samcuks <yarcat@gmail.com>
2012-08-31 01:29:57 +0200

    Removed unused stuff from MainActivity

--- Chemistry Lines/src/com/yarcat/chemistrylines/MainActivity.java
+++ Chemistry Lines/src/com/yarcat/chemistrylines/MainActivity.java
@@ -1,33 +1,22 @@
 package com.yarcat.chemistrylines;
 
-import com.yarcat.chemistrylines.gamelogic.ChemistryLinesGame;
-import com.yarcat.chemistrylines.touchlogic.SlideTouchLogic;
-
-import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.yarcat.chemistrylines.touchlogic.SlideTouchLogic;
 
 public class MainActivity extends Activity {
 
     private SlideTouchLogic mTouchListener;
-    private ChemistryLinesGame mGameLogic;
-    private GestureDetector mGestureDetector;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        RectField field = new RectField(8, 8);
+        // TODO(yarcat): Create RecvField here.
         MainView mainView = new MainView(this, 8, 8);
-        mTouchListener = new SlideTouchLogic(mainView);
-        mGestureDetector = new GestureDetector(this, mTouchListener);
-        mGameLogic = new ChemistryLinesGame(field);
+        mTouchListener = new SlideTouchLogic();
+        mainView.setOnTouchListener(mTouchListener);
         setContentView(mainView);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return mGestureDetector.onTouchEvent(event);
     }
 }
