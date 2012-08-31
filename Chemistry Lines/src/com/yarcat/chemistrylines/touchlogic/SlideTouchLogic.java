d09238366703109bda610d1ecc34c8af4eeefb63
Jaroslavs Samcuks <yarcat@gmail.com>
2012-08-31 02:14:08 +0200

    Initial implementation of SlideTouchLogic
    Source is selected on ACTION_DOWN.
    Destination is selected on ACTION_UP.

--- Chemistry Lines/src/com/yarcat/chemistrylines/touchlogic/SlideTouchLogic.java
+++ Chemistry Lines/src/com/yarcat/chemistrylines/touchlogic/SlideTouchLogic.java
@@ -6,6 +6,22 @@ import android.view.View;
 public class SlideTouchLogic implements View.OnTouchListener {
 
     public boolean onTouch(View v, MotionEvent event) {
+        FieldView fv = (FieldView) v;
+        int n = fv.getIndex(event.getX(), event.getY());
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN:
+            fv.clean();
+            fv.select(n);
+            break;
+        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_CANCEL:
+            fv.clean();
+            fv.clean();
+            break;
+        case MotionEvent.ACTION_MOVE:
+            fv.select(n);
+            break;
+        }
         return true;
     }
 }
