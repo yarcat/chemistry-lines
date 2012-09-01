2cd97d45be0c84c94e29dc53017dac151e34d563
Jaroslavs Samcuks <yarcat@gmail.com>
2012-09-02 01:33:35 +0200

    Use Random.nextInt rather than custom one

--- Chemistry Lines/src/com/yarcat/chemistrylines/algorithms/RandomCell.java
+++ Chemistry Lines/src/com/yarcat/chemistrylines/algorithms/RandomCell.java
@@ -5,12 +5,9 @@ import java.util.Random;
 import com.yarcat.chemistrylines.field.Field;
 
 public class RandomCell {
-    // TODO(before_release) make random generator application wide
-    private static Random random = new Random(0);
 
-    public static int randomInt(int range) {
-        return (int) (random.nextFloat() * range);
-    }
+    // TODO(yarcat): Drop random seed at some point.
+    public static Random random = new Random(0);
 
     public static int getRandomEmptyCell(Field field) {
         int range = countEmptyCells(field);
@@ -19,7 +16,7 @@ public class RandomCell {
         if (range == 0) {
             rv = -1;
         } else {
-            int x = randomInt(range);
+            int x = random.nextInt(range);
             rv = 0;
             while (!field.at(rv).isEmpty() || x > 0) {
                 if (field.at(rv).isEmpty()) {
--- Chemistry Lines/src/com/yarcat/chemistrylines/game/ChemistryLinesGame.java
+++ Chemistry Lines/src/com/yarcat/chemistrylines/game/ChemistryLinesGame.java
@@ -1,6 +1,7 @@
 package com.yarcat.chemistrylines.game;
+
 import static com.yarcat.chemistrylines.algorithms.RandomCell.getRandomEmptyCell;
-import static com.yarcat.chemistrylines.algorithms.RandomCell.randomInt;
+import static com.yarcat.chemistrylines.algorithms.RandomCell.random;
 import static com.yarcat.chemistrylines.field.KnownElements.knownElements;
 
 import com.yarcat.chemistrylines.algorithms.Path;
@@ -32,13 +33,14 @@ public class ChemistryLinesGame implements GameLogic {
     public void addItems() {
         for (int i = 0; i < 3; ++i) {
             int n = getRandomEmptyCell(mField);
-            if (n < 0)
+            if (n < 0) {
                 break;
+            }
             mField.at(n).setElement(getRandomElement());
         }
     }
 
     private Element getRandomElement() {
-        return knownElements.getByIndex(randomInt(knownElements.size()));
+        return knownElements.getByIndex(random.nextInt(knownElements.size()));
     }
 }
