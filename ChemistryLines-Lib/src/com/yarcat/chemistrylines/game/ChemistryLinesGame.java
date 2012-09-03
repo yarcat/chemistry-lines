6ac3dbc04cf1efed407d36a9e8e91436bf225255
Alexey Luchko <luchik@gmail.com>
2012-09-04 00:36:37 +0200

    [Delivers #35357131] Add new elements and remove compounds every turn.

--- ChemistryLines-Lib/src/com/yarcat/chemistrylines/game/ChemistryLinesGame.java
+++ ChemistryLines-Lib/src/com/yarcat/chemistrylines/game/ChemistryLinesGame.java
@@ -4,6 +4,7 @@ import static com.yarcat.chemistrylines.algorithms.RandomCell.getRandomEmptyCell
 import static com.yarcat.chemistrylines.algorithms.RandomCell.random;
 import static com.yarcat.chemistrylines.field.KnownElements.knownElements;
 
+import com.yarcat.chemistrylines.algorithms.FieldCleaner;
 import com.yarcat.chemistrylines.algorithms.Path;
 import com.yarcat.chemistrylines.field.Element;
 import com.yarcat.chemistrylines.field.Field;
@@ -11,15 +12,19 @@ import com.yarcat.chemistrylines.field.Field;
 public class ChemistryLinesGame implements GameLogic {
 
     private Field mField;
+    private FieldCleaner mCleaner;
 
     public ChemistryLinesGame(Field field) {
         mField = field;
+        mCleaner = new FieldCleaner();
     }
 
+    @Override
     public boolean isMoveValid(int origin, int fin) {
         return Path.isReachable(mField, origin, fin);
     }
 
+    @Override
     public void makeMove(int origin, int fin) throws InvalidMove {
         if (!isMoveValid(origin, fin)) {
             throw new InvalidMove();
@@ -28,9 +33,12 @@ public class ChemistryLinesGame implements GameLogic {
                 && mField.at(fin).isEmpty()) {
             mField.at(fin).setElement(mField.at(origin).getElement());
             mField.at(origin).setElement(null);
+            mCleaner.cleanField(mField);
+            addItems();
         }
     }
 
+    @Override
     public void addItems() {
         for (int i = 0; i < 3; ++i) {
             int n = getRandomEmptyCell(mField);
