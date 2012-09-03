f83f5d3ccd4b24ca0e40602c98c054be337c1414
Jaroslavs Samcuks <yarcat@gmail.com>
2012-09-03 23:14:12 +0200

    [#35412149] Forgotten merge

--- Chemistry Lines/tests/com/yarcat/tests/chemistrylines/Registry.java
+++ Chemistry Lines/tests/com/yarcat/tests/chemistrylines/Registry.java
@@ -3,10 +3,7 @@ package com.yarcat.tests.chemistrylines;
 import com.yarcat.chemistrylines.field.Element;
 import com.yarcat.chemistrylines.field.ElementRegistry;
 
-<<<<<<< HEAD
 /** Element registry used for testing */
-=======
->>>>>>> [#35412149] Created a shared library
 class Registry extends ElementRegistry {
     public Element def(String id) {
         Element e = new Element(id, id);
