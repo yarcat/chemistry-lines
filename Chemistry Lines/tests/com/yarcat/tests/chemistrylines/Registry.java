78dd7978fcf3288d909b6ac8531c26d4fa5b05da
Alexey Luchko <luchik@gmail.com>
2012-09-03 22:21:32 +0200

    Fix to previous commit

--- Chemistry Lines/tests/com/yarcat/tests/chemistrylines/Registry.java
+++ Chemistry Lines/tests/com/yarcat/tests/chemistrylines/Registry.java
@@ -3,7 +3,8 @@ package com.yarcat.tests.chemistrylines;
 import com.yarcat.chemistrylines.field.Element;
 import com.yarcat.chemistrylines.field.ElementRegistry;
 
-class TestRegistry extends ElementRegistry {
+/** Element registry used for testing */
+class Registry extends ElementRegistry {
     public Element def(String id) {
         Element e = new Element(id, id);
         register(e);
