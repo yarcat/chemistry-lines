76fef5d34d623576d30498c47195984d39e9925b
Jaroslavs Samcuks <yarcat@gmail.com>
2012-09-10 17:12:42 +0200

    [#35733827] Parse simple wikitable row

--- Tools/src/wikipedia_compound_filter.py
+++ Tools/src/wikipedia_compound_filter.py
@@ -1,4 +1,18 @@
 import html_table_filter
+import collections
+
+
+Synonym = collections.namedtuple("Synonym", "synonym cas_number")
+
+
+class Cell(html_table_filter.Cell):
+
+    def __init__(self):
+        html_table_filter.Cell.__init__(self)
+        self.synonyms = []
+
+    def add_synonym(self, synonym, cas_number):
+        self.synonyms.append(Synonym(synonym, cas_number))
 
 
 class TableFilter(html_table_filter.TableFilter):
@@ -7,4 +21,8 @@ class TableFilter(html_table_filter.TableFilter):
         return dict(attrs).get("class") == "wikitable"
 
     def row_finished(self, row):
-        return len(row)
+        cell = Cell()
+        cell.add_data(row[0].data)
+        cell.add_synonym(row[1].data, row[2].data)
+        row[:] = [cell]
+        return True
--- Tools/tests/test_wikipedia_compound_filter.py
+++ Tools/tests/test_wikipedia_compound_filter.py
@@ -8,6 +8,15 @@ RESOURCE_FILE = os.path.join(os.path.dirname(__file__),
 HTML = open(RESOURCE_FILE).read()
 
 
+SIMPLE_WIKITABLE = """
+<table><tr><td>1</td><td>1</td><td>1</td></tr></table>
+<table class="wikitable"><tr><td>a</td><td>b</td><td>c</td></tr></table>
+"""
+
+def parse_html_as_list(html, parser=None):
+    return [[col.data for col in row] for row in parse_html(html, parser)]
+
+
 def parse_html(html=HTML, parser=None):
     parser = parser or wikipedia_compound_filter.TableFilter()
     parser.feed(html)
@@ -17,9 +26,14 @@ def parse_html(html=HTML, parser=None):
 
 class TestWikipediaCompoundParser(unittest.TestCase):
 
-    def testNoHeaders(self):
-        table = parse_html()
-        self.assertTrue(all(len(row) in (2, 3) for row in table))
+    def testSimpleWikitable(self):
+        table = parse_html(SIMPLE_WIKITABLE)
+        self.assertEquals(len(table), 1)
+        row = table[0]
+        self.assertEquals(len(row), 1)
+        cell = row[0]
+        self.assertEquals(cell.data, "a")
+        self.assertEquals(cell.synonyms, [("b", "c")])
 
 
 if __name__ == "__main__":
