df66b19e2236c351a82155fd90bb58f90b12d3b6
Jaroslavs Samcuks <yarcat@gmail.com>
2012-11-10 01:07:30 +0100

    [#4] Fixed Swing "Formula Mode" case.
    Looks like in Java args in main() contains only command-line options.
    This means that I had to check args[0], not args[1]. Fixed.
    
    Added launchers for "Formula Mode" and "Coumpounds Mode".

--- ChemistryLines-Swing/src/com/yarcat/chemistrylines/swing/ChemistryLines.java
+++ ChemistryLines-Swing/src/com/yarcat/chemistrylines/swing/ChemistryLines.java
@@ -77,7 +77,7 @@ public class ChemistryLines implements Runnable, MouseListener {
 
     public static void main(String[] args) {
         GameFactory factory;
-        if (args.length > 1 && args[1].equals("formula")) {
+        if (args.length > 0 && args[0].equals("formula")) {
             factory = new GameFactory() {
                 @Override
                 public LinesGame newInstance(Field field) {
