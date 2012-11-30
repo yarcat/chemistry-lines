6346efaeaa0ae365be83474796ba69d6af46cbc2
Alexey Luchko <luchik@gmail.com>
2012-12-01 00:16:07 +0200

    [#26] refactored creation of SwingUI

--- Swing/src/com/yarcat/chemistrylines/swing/SwingChemistryLines.java
+++ Swing/src/com/yarcat/chemistrylines/swing/SwingChemistryLines.java
@@ -1,6 +1,8 @@
 package com.yarcat.chemistrylines.swing;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Panel;
@@ -19,8 +21,8 @@ import javax.swing.SwingConstants;
 import com.yarcat.chemistrylines.field.Element;
 import com.yarcat.chemistrylines.field.Field;
 import com.yarcat.chemistrylines.field.RectField;
+import com.yarcat.chemistrylines.game.GameLogic;
 import com.yarcat.chemistrylines.game.GameLogic.InvalidMove;
-import com.yarcat.chemistrylines.game.LinesGame;
 import com.yarcat.chemistrylines.view.SelectionInView;
 
 public class SwingChemistryLines implements MouseListener {
@@ -28,10 +30,9 @@ public class SwingChemistryLines implements MouseListener {
     private final SelectionInView mSelection = new SelectionInView();
 
     private final Field mField;
-    private final LinesGame mGame;
+    private final GameLogic mGame;
     private final Button[] mButtons;
-
-    private Button[] mPreview;
+    private final Button[] mPreview;
 
     @SuppressWarnings("serial")
     private static class Button extends JLabel {
@@ -42,84 +43,141 @@ public class SwingChemistryLines implements MouseListener {
         }
     }
 
-    public static SwingChemistryLines newInstance(GameFactory factory,
-            int cols, int rows) {
-        Field field = new RectField(cols, rows);
-        Button[] buttons = new Button[cols * rows];
+    /** Game UI Factory (! Thread UNSAFE !) */
+    private static class UIFactory {
+        protected int mCols;
+        protected int mRows;
+        protected GameFactory mGameFactory;
+        protected GameLogic mGame;
+        protected Field mField;
+        protected Button[] mButtons;
+        protected Button[] mPreview;
+        protected SwingChemistryLines mGameUI;
 
-        // TODO(luch): global constant for 3.
-        // TODO(luch): use JLabel.
-        Button[] preview = new Button[3];
+        public UIFactory(GameFactory factory, int cols, int rows) {
+            mGameFactory = factory;
+            mCols = cols;
+            mRows = rows;
+        }
 
-        LinesGame gameInstance = factory.newInstance(field);
-        SwingChemistryLines game =
-            new SwingChemistryLines(field, gameInstance, buttons, preview);
+        public SwingChemistryLines newInstance() {
+            mField = new RectField(mCols, mRows);
+            mButtons = new Button[mField.getLength()];
+            // TODO(luch): global constant for 3.
+            mPreview = new Button[3];
 
-        JFrame f = new JFrame("Chemistry Lines - " + factory.getModeName());
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setBackground(Color.BLACK);
-        f.setForeground(Color.WHITE);
-        f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.X_AXIS));
+            mGame = mGameFactory.newInstance(mField);
+            SwingChemistryLines r =
+                mGameUI =
+                    new SwingChemistryLines(mField, mGame, mButtons, mPreview);
 
-        Panel mainPanel = new Panel();
-        f.getContentPane().add(mainPanel);
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+            JFrame f =
+                new JFrame("Chemistry Lines - " + mGameFactory.getModeName());
+            Container rootPane = f.getContentPane();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setBackground(Color.BLACK);
+            f.setForeground(Color.WHITE);
+            // f.setLayout(new BoxLayout(rootPane, BoxLayout.X_AXIS));
 
-        Panel buttonPanel = new Panel(new GridLayout(cols, rows));
-        mainPanel.add(buttonPanel);
-        for (int i = 0; i < cols * rows; ++i) {
-            Button b = new Button(i);
-            buttons[i] = b;
+            fillRootPane(rootPane);
+
+            f.pack();
+            f.setVisible(true);
+
+            mGame.addItems();
+            mGameUI.refresh();
+
+            mGame = null;
+            mField = null;
+            mButtons = null;
+            mPreview = null;
+            mGameUI = null;
+
+            return r;
+        }
+
+        protected void fillRootPane(Container rootPane) {
+            rootPane.setLayout(new BoxLayout(rootPane, BoxLayout.X_AXIS));
+            rootPane.add(createMainPane());
+            rootPane.add(createRightPane());
+        }
+
+        protected Container createMainPane() {
+            Panel p = new Panel();
+            p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+            p.add(createButtonsPane(mButtons));
+            p.add(createPreviewPane(mPreview));
+            return p;
+        }
+
+        protected Container createButtonsPane(Button[] buttons) {
+            Panel buttonPanel = new Panel(new GridLayout(mCols, mRows));
+            for (int i = 0; i < mCols * mRows; ++i) {
+                Button b = createButton(i);
+                b.addMouseListener(mGameUI);
+                buttons[i] = b;
+                buttonPanel.add(b);
+            }
+            return buttonPanel;
+        }
+
+        protected Container createPreviewPane(Button[] preview) {
+            Panel previewPanel = new Panel();
+            previewPanel.setBackground(Color.BLACK);
+            for (int i = 0; i < preview.length; ++i) {
+                Button b = createButton(i);
+                preview[i] = b;
+                previewPanel.add(b);
+            }
+            return previewPanel;
+        }
+
+        protected Button createButton(int n) {
+            Button b = new Button(n);
             b.setPreferredSize(new Dimension(60, 60));
             b.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
             b.setForeground(Color.WHITE);
             b.setHorizontalAlignment(SwingConstants.CENTER);
             b.setOpaque(true);
-            b.addMouseListener(game);
-            buttonPanel.add(b);
+            return b;
         }
 
-        Panel previewPanel = new Panel();
-        previewPanel.setBackground(Color.BLACK);
-        mainPanel.add(previewPanel);
-        for (int i = 0; i < preview.length; ++i) {
-            Button b = new Button(i);
-            preview[i] = b;
-            b.setPreferredSize(new Dimension(60, 60));
-            b.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-            b.setForeground(Color.WHITE);
-            b.setHorizontalAlignment(SwingConstants.CENTER);
-            previewPanel.add(b);
+        protected Container createRightPane() {
+            Panel p = new Panel();
+            p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+            p.add(createLog());
+            return p;
         }
 
-        JTextArea logArea = new JTextArea(10, 10);
-        gameInstance.setGameLogger(new SwingGameLogger(logArea));
-        logArea.setEditable(false);
-        logArea.setBackground(Color.BLACK);
-        logArea.setForeground(Color.WHITE);
+        protected Component createLog() {
+            JTextArea logArea = new JTextArea(10, 10);
+            mGame.setGameLogger(new SwingGameLogger(logArea));
+            logArea.setEditable(false);
+            logArea.setBackground(Color.BLACK);
+            logArea.setForeground(Color.WHITE);
+            return scrollPane(logArea);
+        }
 
-        JScrollPane logScroll = new JScrollPane(logArea);
-        f.getContentPane().add(logScroll);
-        logScroll
-            .setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        logScroll
-            .setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        // logScroll
-        // .setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        // logScroll
-        // .setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-        logScroll.setBorder(BorderFactory.createEmptyBorder());
-
-        f.pack();
-        f.setVisible(true);
-
-        gameInstance.addItems();
-        game.refresh();
-
-        return game;
+        protected Container scrollPane(Component c) {
+            JScrollPane s = new JScrollPane(c);
+            s.setBorder(BorderFactory.createEmptyBorder());
+            // @formatter:off
+            s.setHorizontalScrollBarPolicy(
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+            s.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+            // @formatter:on
+            return s;
+        }
     }
 
-    public SwingChemistryLines(Field field, LinesGame game, Button[] buttons,
+    public static SwingChemistryLines newInstance(GameFactory factory,
+            int cols, int rows) {
+        UIFactory f = new UIFactory(factory, cols, rows);
+        return f.newInstance();
+    }
+
+    public SwingChemistryLines(Field field, GameLogic game, Button[] buttons,
             Button[] preview) {
         mField = field;
         mGame = game;
