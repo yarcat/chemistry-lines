260994def3a624102906e95494ba932dc6dd2d86
Alexey Luchko <luchik@gmail.com>
2012-12-15 22:38:16 +0200

    Generate KnownFormulas with --no-hydrates by default

--- Tools/gen.sh
+++ Tools/gen.sh
@@ -2,7 +2,7 @@
 #  * does not match '()' and '[]' of --pair-brackets
 #  * does not match anything with --closing-brackets
 known_formulas_cmd="python2.7 -B src/wiki_chemical_dict.py \
-    -C --max-coefficient=4 \
+    -C --max-coefficient=4 --no-hydrates \
     -period 1 -period 2 -period 3 -period 4 \
     -atom Ag -atom Au -atom I \
     tests/data/dictionary_of_chemical_formulas.html"
